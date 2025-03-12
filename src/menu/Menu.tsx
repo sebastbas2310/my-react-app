aa9fb953198d301bd6cdf6664cb679edbc61cf8d
sebastbas2310 <165617545+sebastbas2310@users.noreply.github.com>
2025-03-11 21:18:53 -0500

    arreglando bugs

--- src/menu/Menu.tsx
+++ src/menu/Menu.tsx
@@ -7,13 +7,13 @@ export const Menu = () => {
       <nav className="bg-gradient-to-r from-purple-800 to-purple-900 py-4 px-6 flex justify-center">
       <div className="bg-purple-700 px-6 py-2 rounded-full flex space-x-6 shadow-lg">
         {["Inicio", "Lógica", "Clase", "Configuración"].map((item) => (
-          <a
+          <Link 
             key={item}
-            href="/Quiz" 
+            to="/Principal"
             className="text-white font-semibold px-4 py-2 rounded-lg hover:bg-purple-500 transition"
           >
             {item}
-          </a>
+          </Link>
         ))}
       </div>
     </nav>

9aafca4b9319ec35e63006c0cf232e0dffcca9ff
Pablo T <licenciadotyr@gmailcom>
2025-03-26 12:57:27 -0500

    Contenido Teorico Añadido

--- src/components/CursoBool.tsx
+++ src/components/CursoBool.tsx
@@ -34,6 +34,46 @@ export const CursoBool = () => {
             <img src="https://matediscretasu3.files.wordpress.com/2011/10/negacion2.jpg" alt="Bicondicional" className="w-auto h-auto max-w-full rounded-lg"/>
         </div>
         
+        <div className="bg-gray-800 rounded-xl p-6 my-4 flex items-center justify-between">
+            <div className="w-3/5 text-left">
+                <h2 className="text-xl font-semibold">Condicional (→)</h2>
+                <p>El condicional es verdadero excepto cuando la primera proposición es verdadera y la segunda es falsa. Se representa con el símbolo →.</p>
+            </div>
+            <img src="https://www.matesfacil.com/wp-content/uploads/2015/06/condicional.png" alt="Condicional" className="w-auto h-auto max-w-full rounded-lg"/>
+        </div>
+
+        <div className="bg-gray-800 rounded-xl p-6 my-4 flex items-center justify-between">
+            <div className="w-3/5 text-left">
+                <h2 className="text-xl font-semibold">Bicondicional (↔)</h2>
+                <p>El bicondicional es verdadero cuando ambas proposiciones tienen el mismo valor de verdad. Se representa con el símbolo ↔.</p>
+            </div>
+            <img src="https://www.matesfacil.com/wp-content/uploads/2015/06/bicondicional.png" alt="Bicondicional" className="w-auto h-auto max-w-full rounded-lg"/>
+        </div>
+
+        <div className="bg-gray-800 rounded-xl p-6 my-4 flex items-center justify-between">
+    <div className="w-3/5 text-left">
+        <h2 className="text-xl font-semibold">Tablas de la Verdad</h2>
+        <p>Una Tabla de la verdad se forma con el proposito de acomodar los valores de las variables dadas segun sea la operacion, esta se puede presentar de dos formas, Binaria Y De Proposicion Compuesta.</p>
+    </div>
+
+    <div className="w-2/5 flex flex-col space-y-4">
+
+    <div className="flex flex-col items-center">
+            <h3 className="text-lg font-semibold">Forma Binaria </h3>
+            <img src="https://www.matesfacil.com/wp-content/uploads/2015/06/bicondicional.png" alt="Bicondicional" className="w-auto h-auto max-w-full rounded-lg"/>
+            <p className="text-sm text-gray-400">Verdadero o Falso,(Como las anteriormente mostradas)</p>
+        </div>
+        <div className="flex flex-col items-center">
+            <h3 className="text-lg font-semibold">Proposición Compuesta</h3>
+            <img src="https://i.ytimg.com/vi/-A_MM2trMWc/maxresdefault.jpg" alt="Bicondicional" className="w-auto h-auto max-w-full rounded-lg"/>
+            <p className="text-sm text-gray-400">1 o 0,(Tabla de la Verdad de la Disyuncion (v)) .</p>
+        </div>
+    </div>
+
+        
+</div>
+
+
         <div className="my-6">
             <Link to="/PruebaBool" className="px-6 py-3 bg-purple-600 text-white font-bold rounded-lg hover:bg-purple-700" >Ir a la Prueba</Link>
         </div>
